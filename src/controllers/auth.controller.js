113e8d51f42afafae19ac0b384eee0236295d374
Diego <diegocomisso@gmail.com>
2026-05-14 11:29:01 -0300

    Added register method to controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,64 @@
 const User = require('../models/User')
 
+exports.register = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+
+        // verificar si ya existe
+        const existe = await User.findOne({
+            where: { email }
+        })
+
+        if (existe) {
+            return res.status(400).json({
+                message: 'El usuario ya existe'
+            })
+        }
+
+        // crear usuario
+        const newUser = await User.create({
+            email,
+            password
+        })
+
+        res.status(201).json({
+            message: 'Usuario creado',
+            user: {
+                id: newUser.id,
+                email: newUser.email
+            }
+        })
+
+    } catch (error) {
+        console.error(error)
+
+        res.status(500).json({
+            message: 'Error del servidor'
+        })
+    }
+}
+
+
 exports.login = async (req, res) => {
     const { email, password } = req.body
 
     try {
-        const user = await User.findOne({ where: { email } })
+        const user = await User.findOne({ 
+            where: { email } 
+        })
 
         if (!user) {
-            return res.status(404).json({ message: 'Usuario no encontrado' })
+            return res.status(404).json({ 
+                message: 'Usuario no encontrado' 
+            })
         }
 
         // comparacion simple (DESPUES REPASAR COMO USAR BCRYPT)
         if (user.password !== password) {
-            return res.status(401).json({ message: 'Contraseña incorrecta' })
+            return res.status(401).json({ 
+                message: 'Contraseña incorrecta' 
+            })
         }
 
         res.json({
@@ -25,6 +71,9 @@ exports.login = async (req, res) => {
 
     } catch (error) {
         console.error(error)
-        res.status(500).json({ message: 'Error del servidor' })
+
+        res.status(500).json({ 
+            message: 'Error del servidor' 
+        })
     }
 }
