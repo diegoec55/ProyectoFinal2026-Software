eb150d2843a83c80f149b56c7191512438b60a1a
Diego <diegocomisso@gmail.com>
2026-05-14 11:31:59 -0300

    added register route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/auth.controller')
 
+//login
 router.post('/login', controller.login) //probando primera ruta al login
+
+//register
+router.post('/register', controller.register)
 
 module.exports = router
