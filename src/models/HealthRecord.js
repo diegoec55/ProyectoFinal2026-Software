4b4a0305e3ea56a4826b862896659688a94c3132
Diego <diegocomisso@gmail.com>
2026-06-01 18:54:36 -0300

    Deleted last changes

--- src/models/HealthRecord.js
+++ src/models/HealthRecord.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize')
 const { sequelize } = require('../config/database')
-const User = require('./User')
 
 const HealthRecord = sequelize.define('HealthRecord', {
 
@@ -8,16 +7,6 @@ const HealthRecord = sequelize.define('HealthRecord', {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
-    },
-
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: User,
-            key: 'id'
-        },
-        onDelete: 'CASCADE'
     },
 
     heart_rate: {
@@ -43,9 +32,5 @@ const HealthRecord = sequelize.define('HealthRecord', {
 }, {
     timestamps: true
 })
-
-// Relaciones
-User.hasMany(HealthRecord, { foreignKey: 'userId' })
-HealthRecord.belongsTo(User, { foreignKey: 'userId' })
 
 module.exports = HealthRecord
