4f5b06931322b4e17e714dd023b6bc0c26373fd0
Diego <diegocomisso@gmail.com>
2026-06-01 14:43:04 -0300

    modelo sequelize healthrecord.js creado

---
+++ src/models/HealthRecord.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../config/database')
+
+const HealthRecord = sequelize.define('HealthRecord', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    heart_rate: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    blood_oxygen: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+})
+
+module.exports = HealthRecord
