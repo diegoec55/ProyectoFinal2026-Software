500d28c5a5693f7baeff3a21d52875e2fc176ed9
Diego <diegocomisso@gmail.com>
2026-06-08 17:42:23 -0300

    Changed the destination HTML in register and login, destination index.html

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api', healthRoutes)
 
-//ruta principal
+// ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/login.html'))
 })
