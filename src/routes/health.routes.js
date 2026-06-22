ea3aa0510dd07512bbb8b9fbc667844548238095
Diego <diegocomisso@gmail.com>
2026-06-22 13:20:03 -0300

    Modified endpoint for the frontend on line 9 of health.routes.js

--- src/routes/health.routes.js
+++ src/routes/health.routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const healthController = require('../controllers/health.controller')
 
-const healthController =
-    require('../controllers/health.controller')
-
+// Endpoint para que el ESP32 o Postman sigan guardando datos
 router.post('/data', healthController.createRecord)
 
-router.get('/data', healthController.getRecords)
+// ENDPOINT MODIFICADO: Coincide con la peticion del frontend
+router.get('/health/user/:userId', healthController.getRecords)
 
 module.exports = router
