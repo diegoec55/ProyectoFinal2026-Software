3cdc1d9e844f8df85b02313938a408c50e3f08c8
Diego <diegocomisso@gmail.com>
2026-06-22 13:28:58 -0300

    Ordering the order of relationships in index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,26 +1,27 @@
 const User = require('./User')
 const HealthRecord = require('./HealthRecord')
 
-User.hasMany(HealthRecord, {
-    foreignKey: 'user_id',
-    as: 'records'
-})
-
-HealthRecord.belongsTo(User, {
-    foreignKey: 'user_id',
-    as: 'user'
-})
-
-// NUEVA RELACIÓN: Autoreferencia para el cuidador asignado
+// Relacion Autoreferencia del cuidador (Carer) - Se ejecuta sobre la misma tabla
 User.belongsTo(User, { 
     foreignKey: 'carerId', 
     as: 'assignedCarer' 
 })
 
-// Opcional: Si querés saber a qué usuarios cuida un cuidador específico
 User.hasMany(User, {
     foreignKey: 'carerId',
     as: 'patients'
+})
+
+// Relacion con HealthRecord - apunta al objeto del Modelo User
+User.hasMany(HealthRecord, {
+    foreignKey: 'user_id',
+    as: 'records',
+    onDelete: 'CASCADE'
+})
+
+HealthRecord.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user'
 })
 
 module.exports = {
