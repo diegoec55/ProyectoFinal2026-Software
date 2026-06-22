9a19bf53a1331520c7e2e738c1364f6771ea533c
Diego <diegocomisso@gmail.com>
2026-06-22 11:52:06 -0300

    Added more data fields to user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,23 +1,29 @@
-const User = require('../models/User')
+// importamos desde el index de modelos
+const { User } = require('../models/index') 
 
 // obtener usuario
 exports.getUser = async (req, res) => {
 
     try {
-        const user = await User.findByPk(req.params.id)
+        const user = await User.findByPk(req.params.id, {
+            // Ocultamos la contraseña por seguridad
+            attributes: { exclude: ['password'] }, 
+            // Traemos los datos basicos de su cuidador asociado
+            include: {
+                model: User,
+                as: 'assignedCaregiver',
+                attributes: ['id', 'name', 'lastName', 'email'] 
+            }
+        })
 
         if (!user) {
-            return res.status(404).json({
-                message: 'Usuario no encontrado'
-            })
+            return res.status(404).json({message: 'Usuario no encontrado'})
         }
         res.json(user)
 
     } catch (error) {
         console.error(error)
-        res.status(500).json({
-            message: 'Error del servidor'
-        })
+        res.status(500).json({message: 'Error del servidor'})
     }
 }
 
@@ -28,13 +34,20 @@ exports.updateUser = async (req, res) => {
         const user = await User.findByPk(req.params.id)
 
         if (!user) {
-            return res.status(404).json({
-                message: 'Usuario no encontrado'
-            })
+            return res.status(404).json({message: 'Usuario no encontrado'})
         }
-        user.name = req.body.name
+        // Permitimos actualizar los campos personales, enfermedades y el cuidador asignado
+        user.name = req.body.name || user.name
+        user.lastName = req.body.lastName || user.lastName
+        user.dni = req.body.dni || user.dni
+        user.birthDate = req.body.birthDate || user.birthDate
+        user.illnesses = req.body.illnesses !== undefined ? req.body.illnesses : user.illnesses
+        user.caregiverId = req.body.caregiverId !== undefined ? req.body.caregiverId : user.caregiverId
 
         await user.save()
+
+        // Ocultamos la contraseña antes de responder
+        user.password = undefined
 
         res.json({
             message: 'Usuario actualizado',
@@ -43,8 +56,6 @@ exports.updateUser = async (req, res) => {
 
     } catch (error) {
         console.error(error)
-        res.status(500).json({
-            message: 'Error del servidor'
-        })
+        res.status(500).json({message: 'Error del servidor'})
     }
 }
