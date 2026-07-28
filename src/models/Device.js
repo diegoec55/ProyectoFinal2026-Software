6f533695f23c449512e1844584b03db5cea71aaa
Diego <diegocomisso@gmail.com>
2026-07-28 00:18:55 -0300

    Changed to allow patient-free devices

--- src/models/Device.js
+++ src/models/Device.js
@@ -36,7 +36,7 @@ const Device = sequelize.define(
         },
         user_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             references: {
             model: 'users',
             key: 'id'
