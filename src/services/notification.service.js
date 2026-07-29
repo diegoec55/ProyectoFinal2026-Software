0ac0ea4c5e546bcff6619fa26cd4a5f72cbd8323
Diego <diegocomisso@gmail.com>
2026-07-29 20:05:02 -0300

    The sedWhatsapp function has been modified to avoid problems if a user types spaces, hyphens, or the prefix 0.

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -5,8 +5,17 @@ exports.sendWhatsApp = async (phone, message) => {
 
     try {
         const apiKey = process.env.PHONE_API_KEY
+
+        // Eliminar todo lo que no sea un número
+        phone = phone.replace(/\D/g, '')
+
+        // Si comienza con 0, quitarlo
+        if (phone.startsWith('0')) {
+            phone = phone.substring(1)
+        }
+        
         const url =
-            `https://api.callmebot.com/whatsapp.php?phone=+549${phone}&text=${encodeURIComponent(message)}&apikey=${apiKey}`
+            `https://api.callmebot.com/whatsapp.php?phone=549${phone}&text=${encodeURIComponent(message)}&apikey=${apiKey}`
 
         const response = await axios.get(url)
 
